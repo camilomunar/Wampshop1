16c6bfed160b52082de494631ff1cc4d24f608dd
Camilo munar <cristiankmilomunar@gmail.com>
2018-05-04 09:35:23 -0500

    Se realiza la conexion a la base de datos

---
+++ db/Conexion.php
@@ -0,0 +1,28 @@
+<?php
+
+class Conexion{
+
+    static public function conectar(){
+
+        $db_host = "Localhost"; 
+        $usuario ="root";
+        $pass = "";
+        $db_nombre = "wampshop";
+
+        $conexion = mysqli_connect($db_host,$usuario,$pass,$db_nombre);
+
+        if($conexion->connect_errno){
+
+            echo $conexion->connect_errno . "error en la conexion";
+
+            return $conexion;
+        }else{
+            echo "Excelente conexion";
+        }
+        $conexion->set_charset('utf8');
+
+    }
+}
+Conexion::conectar();
+
+?>
